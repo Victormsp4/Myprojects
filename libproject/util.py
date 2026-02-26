2b78106e3f630908dfb2263efea07801a0db4ade
victor <victor81673408@gmail.com>
2026-02-26 12:42:31 -0300

    first commit

---
+++ libproject/util.py
@@ -0,0 +1,29 @@
+from random import randint
+class biblioteca:
+    #cria um obj biblioteca
+    def __init__(self,nome,banco_livros):
+        self.banco_livros = {1000:'d'}
+        self.nome = nome
+    
+    #adiciona um livro, verifica se e uma instancia de string e padroniza em minuculo sem espaços
+    def add_book(self,id,nome):
+        if isinstance(name, str):
+            name = name.strip().lower()
+            if nome not in self.banco_livros:
+                return self.banco_livros[id:nome].update()
+  
+    # gera um id unico para um novo livro
+    def gera_id(self):
+        getid = randint(1000,1999)
+        if getid not in self.banco_livros:
+            return getid
+        else:
+            print('ja ta la')
+    
+    def verifica(self, name):
+            if isinstance(name, str):
+                name = name.strip().lower()
+                return name
+
+b1 = biblioteca(3333,'')
+print(b1.gera_id())
